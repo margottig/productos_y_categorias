13af5a709050955cd71cbf19f29088e737682b39
margottig <marchelandio@gmail.com>
2023-05-19 08:43:35 -0500

    creacion modeloes

---
+++ src/main/java/com/example/productosycategorias/models/ProductosCategorias.java
@@ -0,0 +1,90 @@
+package com.example.productosycategorias.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="productoscategorias")
+public class ProductosCategorias {
+	   @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    
+	    @Column(updatable=false)
+	    private Date createdAt;
+	    private Date updatedAt;
+	    
+	    @ManyToOne(fetch = FetchType.LAZY)
+	    @JoinColumn(name="product_id")
+	    private Producto product;
+	    
+	    @ManyToOne(fetch = FetchType.LAZY)
+	    @JoinColumn(name="category_id")
+	    private Categoria category;
+	    
+	    public ProductosCategorias() {
+	        
+	    }
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Date getCreatedAt() {
+			return createdAt;
+		}
+
+		public void setCreatedAt(Date createdAt) {
+			this.createdAt = createdAt;
+		}
+
+		public Date getUpdatedAt() {
+			return updatedAt;
+		}
+
+		public void setUpdatedAt(Date updatedAt) {
+			this.updatedAt = updatedAt;
+		}
+
+		public Producto getProducttwo() {
+			return product;
+		}
+
+		public void setProducttwo(Producto producttwo) {
+			this.product = producttwo;
+		}
+
+		public Categoria getCategorytwo() {
+			return category;
+		}
+
+		public void setCategorytwo(Categoria categorytwo) {
+			this.category = categorytwo;
+		}
+
+		@PrePersist
+		protected void onCreate() {
+			this.createdAt = new Date();
+		}
+		@PostPersist
+		protected void onUpdate() {
+			this.updatedAt = new Date();
+		}
+	    
+	    
+}
